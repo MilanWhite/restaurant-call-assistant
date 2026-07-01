02712c156d1f2571e1e5a3779de170c87d8f8ec0
Milan White <milan.white.25@gmail.com>
2026-07-01 16:42:32 -0400

    [MW] Style La Veranda interface

--- app/src/main/java/com/example/restaurant_call_assistant/ui/theme/Color.kt
+++ app/src/main/java/com/example/restaurant_call_assistant/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.restaurant_call_assistant.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AppBlack = Color(0xFF050505)
+val AppWhite = Color(0xFFFFFFFF)
+val AppBeige = Color(0xFFE5CFBD)
+val AppDarkBeige = Color(0xFF684E42)
+val AppLine = Color(0xFFE6E2DE)
+val AppMuted = Color(0xFF68615D)
