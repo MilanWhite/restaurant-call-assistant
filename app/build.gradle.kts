5fab1df654145bbe10f99d1201de43f6685dc128
Milan White <milan.white.25@gmail.com>
2026-07-23 21:30:56 -0400

    [MW] Release version 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.restaurant_call_assistant"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 21
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
